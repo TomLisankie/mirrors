8cdce59a069a3f0222aba0bf29b4b27fb40e8621
Thomas Lisankie <link285@gmail.com>
2014-07-04 22:26:47 -0400

    Added some functionality to Sprite class

--- Mirrors Game/src/gameobjects/Sprite.java
+++ Mirrors Game/src/gameobjects/Sprite.java
@@ -1,7 +1,17 @@
 package gameobjects;
 
+import java.awt.image.*;
+
 public class Sprite {
 	
 	//represents image of a Character
+	
+	private BufferedImage image;
+	
+	public void setImage(BufferedImage img){
+		
+		image = img;
+		
+	}
 
 }
